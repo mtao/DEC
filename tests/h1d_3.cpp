81b2c6b69a194b99cfac64b96139b2b44302aeb1
Michael Tao <mtao@dgp.toronto.edu>
2013-03-08 12:31:24 -0500

    more files i forgot to transfer

---
+++ tests/h1d_3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "simplicialComplex.hpp"
+#include "io.hpp"
+#include "dec.hpp"
+
+class DECTest: public DEC<TriangleMesh>
+{
+    public:
+    DECTest(const TriangleMesh & sc): DEC<TriangleMesh>(sc)
+    {
+    }
+    bool check_rows() {
+        auto&& d = d<0>();
+        return (d.rows() == 3 && d.cols() == 4);
+    }
+
+
+};
+
+int main()
+{
+    typedef Eigen::Vector3d V;
+    std::vector<V> vecs;
+    std::vector<mtao::IndexSet<3> > tris;
+    for(int i=0; i < 10; ++i)
+    {
+        vecs.push_back(V(i,i,i));
+    }
+    vecs[0] = V(0,0,0);
+    vecs[3] = V(1,1,0);
+    vecs[5] = V(1,0,0);
+    vecs[6] = V(0,1,0);
+    tris.push_back(mtao::IndexSet<3>({0,3,5}));
+    tris.push_back(mtao::IndexSet<3>({3,0,6}));
+
+    TriangleMesh sc(tris,vecs);
+    DECTest dec(sc);
+
+
+    return 0;
+}
